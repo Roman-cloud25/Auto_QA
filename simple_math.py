8012d06ac072eb3eb7cc20bb6a3117c35403cfcd
Roman-cloud25 <roman.sergatiy@gmail.com>
2026-02-17 07:27:30 +0100

    Delete simple_math.py

--- simple_math.py
+++
@@ -1,7 +0,0 @@
-class SimpleMath:
-
-    def square(self, x): # Returns the square of a number
-        return x * x
-
-    def cube(self, x): # Returns the cube of a number
-        return x * x * x
