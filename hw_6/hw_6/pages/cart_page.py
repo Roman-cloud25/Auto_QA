f9f82de188becf4ddcfe8d68c5da43de7dd886ed
Roman <roman.sergatiy@gmail.com>
2026-03-26 21:05:11 +0100

    Delete hw_6 directory
    fix

--- hw_6/hw_6/pages/cart_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-from .base_page import BasePage
-
-
-class CartPage(BasePage):
-    CHECKOUT_BUTTON = (By.ID, "checkout")
-
-    def proceed_to_checkout(self):
-        self.click(self.CHECKOUT_BUTTON)
